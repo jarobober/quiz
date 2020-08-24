9d530f3a32ae270c92faecfe860beaa0bc4f052d
jarobober <jarek.bober@gmail.com>
2020-08-24 18:36:02 +0200

    update

--- script.js
+++ script.js
@@ -157,41 +157,50 @@ const questions = [
             { text: "I would sue him.", person: "arturito"}
         ]
     },
+    {
+        question: "Where would you go if you had to hide away?",
+        answers: [
+            { text: "Somewhere in Central America.", person: "tokio"},
+            { text: "I think i will never have to hide.", person: "arturito"},
+            { text: "Some Asian island.", person: "professor"},
+            { text: "South America.", person: "helsinki"}
+        ]
+    }
 ]
 
 const tokio = {
     name: "Silene TOKIO Oliveira",
-    photo: "https://3.bp.blogspot.com/-ZeadMZYcwSo/XorLMm-CGsI/AAAAAAAAXHE/LGfD1otNbxsQia9nQk-TwAlz4Qli7R3lwCLcBGAsYHQ/w600-h600-p-k-no-nu/money-heist-vault-tokio-ursula-corbero-uhdpaper.com-4K-6.1115-wp.thumbnail.jpg",
+    photo: "./photos/tokio.png",
     info: "Tokyo is reckless and impulsive, she usually acts before she thinks, which sometimes results in things she regrets. She is quick to lose her patience, as well as sometimes being shown to quickly draw conclusions. She can’t control her emotions.",
 };
 
 const professor = {
     name: "Sergio EL PROFESOR Marquina",
-    photo: "https://fsa.zobj.net/crop.php?r=gEAPy38-B64Ak-Bs0hA5CRIzRgmGun8YzC6dw9X1SUPcfCR21WhZ-9Yj6YWmviDf6I8nuK-0YxqUIK7AD_ZWF4fESHWLQj9hHARuIlKgEwWyTGrLWP5sRxOA8efR9CugM3UxQ9n8aEi_EHT_",
+    photo: "./photos/prof.jpg",
     info: "Sergio is highly intelligent and meticulous. He appears to be very determined to pull off the greatest money heist in the world. He had been planning for it for several years.",
 };
 
 const helsinki = {
     name: "Yashin HELSINKI Dasáyev",
-    photo: "https://vignette.wikia.nocookie.net/money-heist/images/f/fd/MH_Part_3_Helsinki_Mask_Still.jpg/revision/latest?cb=20200313171435",
+    photo: "./photos/helsinki.jpg",
     info: "Helsinki (Darko Perić) has a dark past that's little spoken about. Born in Serbia, he was a soldier in the Yugoslav Wars and has been described as a war criminal.",
 };
 
 const arturito = {
     name: "Arturo ARTURITO Román",
-    photo: "https://vignette.wikia.nocookie.net/haus-des-geldes/images/9/92/Arturo_Roman.jpg/revision/latest?cb=20180427220213&path-prefix=de",
+    photo: "./photos/arturo.jpg",
     info: "He was the general director of the Royal Mint of Spain before the heist took place. As one of the hostages, Arturo constantly got himself in trouble due to his attempts to defy the robbers and stage uprisings within the Royal Mint. After the heist, he became something of a celebrity, giving talks about his experience.",
 };
 
 const berlin = {
     name: "Andrés BERLIN de Fonollosa",
-    photo: "https://i.pinimg.com/originals/71/7a/1f/717a1f42ae00bf54a2dbeb695d37602d.jpg",
+    photo: "./photos/berlin.jpg",
     info: "Berlin is arrogant, narcissistic and seems to be a psychopath, showing multiple symptoms such as superficial charm and lack of empathy, proven as he coldly ordered the execution of a pregnant woman.",
 };
 
 const nairobi = {
     name: "Ágata NAIROBI Jímemez",
-    photo: "https://d.newsweek.com/en/full/1578556/money-heist-season-4.jpg?w=1600&h=1600&q=88&f=89398f05b610061739be6d8349330c31",
+    photo: "./photos/nairobi.jpg",
     info: "Nairobi is enthusiastic, motivating and always active. She usually speaks loud and clear and does her job, leading the production of money very well because of these skills. Out of the other robbers, she seems to be most kind hearted and empathetic as seen in tendency to be easily affected by an emotional situation or other people's actions.",
 };
 
